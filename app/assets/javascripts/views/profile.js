2a43af2d68d3c63fee52eb68fea8f5a70ccb91b7
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-28 18:13:32 -0400

    Add backbone event to remove profile pop up view

--- app/assets/javascripts/views/profile.js
+++ app/assets/javascripts/views/profile.js
@@ -4,9 +4,16 @@ app.ProfileView = Backbone.View.extend({
   tagName: 'div',
   className: 'profileContainer',
   template: $('#profileTemplate').html(),
+  events: {
+    'click .card': 'shrinkProfile'
+  },
   render: function() {
     var tmpl = _.template(this.template);
     this.$el.html(tmpl(this.model.toJSON()));
     return this;
+  },
+  shrinkProfile: function() {
+    console.log('test');
+    $('#profile').html('');
   }
 });
